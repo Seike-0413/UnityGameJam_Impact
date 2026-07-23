6a9b2b08e09eb274fb56dca6d8b337d100cc45d0
Nishida Kohei <ca01264024@st.kawahara.ac.jp>
2026-07-23 15:24:30 +0900

    Title_完成

--- Assets/Script/Title.cs
+++ Assets/Script/Title.cs
@@ -1,14 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void Start()
     {
-        
+        Debug.Log("Startボタンが押された");
+        SceneManager.LoadScene("SampleScene");
     }
 
     // Update is called once per frame
