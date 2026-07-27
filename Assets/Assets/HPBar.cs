0c54343793a0c83891bb6c89221d86906e63f914
Uchimasu shunsuke <Ca01264005@st.kawahara.ac.jp>
2026-07-27 13:36:58 +0900

    PlayerのHPUIの実装

---
+++ Assets/Assets/HPBar.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerHP : MonoBehaviour
+{
+    public Slider hpBar;
+    public TextMeshProUGUI hpText;
+
+    public int maxHP = 100;
+    int hp;
+
+    void Start()
+    {
+        hp = maxHP;
+        UpdateHP();
+    }
+
+    public void Damage(int damage)
+    {
+        hp -= damage;
+
+        if (hp < 0)
+            hp = 0;
+
+        UpdateHP();
+    }
+
+    void UpdateHP()
+    {
+        hpBar.value = hp;
+        hpText.text = hp + " / " + maxHP;
+        Damage(20);
+    }
+}
